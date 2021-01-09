1a4ee29a24b77c0c99810e80bb32fac7f8b2788a
Sardor <asrorjon5538@gmail.com>
2021-01-09 20:49:56 +0500

    second commit

--- assets/script/app.js
+++ assets/script/app.js
@@ -5,3 +5,69 @@ var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-tog
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
     return new bootstrap.Dropdown(dropdownToggleEl)
 })
+$('.wided').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    nextArrow: $('.prev-arrow'),
+    prevArrow: $('.next-arrow'),
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+$('.sale').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    nextArrow: $('.prev-trigger'),
+    prevArrow: $('.next-trigger'),
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
