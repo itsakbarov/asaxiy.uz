d725f1896c9799106b358eeb680020d985e7e731
Sardor <asrorjon5538@gmail.com>
2021-01-10 12:06:31 +0500

    carousel initialized

--- assets/script/app.js
+++ assets/script/app.js
@@ -143,3 +143,18 @@ $('.brands').slick({
         }
     ]
 });
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    $('#back-to-top').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
