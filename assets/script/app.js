596b94fb2c2738852e22672cb61e17a29068a9af
Sardor <asrorjon5538@gmail.com>
2021-01-11 08:25:05 +0500

    Mobile item list added

--- assets/script/app.js
+++ assets/script/app.js
@@ -19,7 +19,7 @@ $('.wided').slick({
                 slidesToShow: 3,
                 slidesToScroll: 3,
                 infinite: true,
-                dots: true
+                dots: false
             }
         },
         {
@@ -59,7 +59,7 @@ $('.sale').slick({
                 slidesToShow: 3,
                 slidesToScroll: 3,
                 infinite: true,
-                dots: true
+                dots: false
             }
         },
         {
@@ -92,7 +92,7 @@ $('.accessories').slick({
                 slidesToShow: 3,
                 slidesToScroll: 3,
                 infinite: true,
-                dots: true
+                dots: false
             }
         },
         {
@@ -121,23 +121,25 @@ $('.brands').slick({
     responsive: [{
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 4,
                 slidesToScroll: 3,
                 infinite: true,
-                dots: true
+                dots: false
             }
         },
         {
             breakpoint: 600,
             settings: {
-                slidesToShow: 2,
+                dots: false,
+                slidesToShow: 3,
                 slidesToScroll: 2
             }
         },
         {
             breakpoint: 480,
             settings: {
-                slidesToShow: 1,
+                dots: false,
+                slidesToShow: 2,
                 slidesToScroll: 1
             }
         }
